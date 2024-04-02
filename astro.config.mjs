b0910a31e315ad415d3fdacbb3cb853acf25e4d5
mariosantos2003-dot <mariosantosbl2003@gmail.com>
2024-04-02 10:16:14 +0200

    header mejorado con shrink on scroll

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-
+  include: ["**/*.html", "**/*.css", "**/*.js", "**/*.otf"], // Incluye la fuente en la construcción
 });
